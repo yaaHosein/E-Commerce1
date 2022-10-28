78b61b771b8ac41b68613145a2decf82964c9c60
yaaHosein <layali.14.peshawar@hotmail.com>
2022-10-28 14:30:48 +0330

    removing Items component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import React, { useState } from "react";
-
 import data from "./data";
 import BasketView from "./BasketView";
 export default function App() {
     const [items, setItems] = useState(data);
-    const Items = ({ items }) => {
-        return (
-            <>
-                {items.map((item) => {
-                    const { id, name, image, price, quantity, description } = item;
-                    return (
-                        <div key={id}>
-                            <div className="details">
-                                <h3> {name}</h3>
-                                <img src={image} alt={name} />
-                                <p>Quantity: {quantity} </p>
-                                <p>Price: {price}$</p>
-                                <p>Description: {description} </p>
-                            </div>
-                            <button
-                                disabled={items.quantity === 0}
-                                onClick={() => setItems(<BasketView BasketView={BasketView} />)}
-                                className="btn"
-                            >
-                                Add to basket
-                            </button>
-                        </div>
-                    );
-                })}
-            </>
-        );
-    };
-    return (
-        <Items items={items} />
-  );
+    return <>
+        {items.map((item) => {
+            const { id, name, image, price, quantity, description } = item;
+            return (
+                <div key={id}>
+                    <div className="details">
+                        <h3> {name}</h3>
+                        <img src={image} alt={name} />
+                        <p>Quantity: {quantity} </p>
+                        <p>Price: {price}$</p>
+                        <p>Description: {description} </p>
+                    </div>
+                    <button
+                        disabled={items.quantity === 0}
+                        onClick={() => setItems(<BasketView BasketView={BasketView} />)}
+                        className="btn"
+                    >
+                        Add to basket
+                    </button>
+                </div>
+            );
+        })}
+    </>
+
 }
