ddf04e702b06ff5025e5298ff9b2de602c0d4177
yaaHosein <layali.14.peshawar@hotmail.com>
2022-10-31 12:35:20 +0330

    shopping cart

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React, { useState } from "react";
-import data from "./data";
+import { React, useState, useEffect } from "react";
 import BasketView from "./BasketView";
-export default function App() {
-    const [items, setItems] = useState(data);
-    const [basket, updateBasket] = useState([]);
-    return <>
-        {items.map((item) => {
-            const { id, name, image, price, quantity, description } = item;
-            return (
-                <div key={id}>
-                    <div className="details">
-                        <h3> {name}</h3>
-                        <img src={image} alt={name} />
-                        <p>Quantity: {quantity} </p>
-                        <p>Price: {price}$</p>
-                        <p>Description: {description} </p>
-                    </div>
-                    <button
-                        disabled={items.quantity === 0}
-                        onClick={() => updateBasket()(<BasketView />)}
-                        className="btn"
-                    >
-                        Add to basket
-                    </button>
-                </div>
-            );
-        })}
-    </>
+import data from "./data";
 
-}
+export default function App() {
+  const [items, setItems] = useState(data);
+  const [basket, updateBasket] = useState([]);
+
+  useEffect(() => {
+    console.log("product added", [items])
+  })
+    return (
+      <>
+        <BasketView items={items} />
+        {basket}
+        <button
+          disabled={items.quantity === 0}
+          onClick={() => updateBasket([])}
+          className="btn">
+          Add to basket
+        </button>
+      </>
+
+  )
+};
--- src/BasketView.js
+++ src/BasketView.js
@@ -1,6 +1,27 @@
-// import React from "react"
-// import BasketView from "./BasketView"
+import React from "react";
 
-// <h1>No product in your basket</h1>
-// export default BasketView
+const BasketView = ({ items }) => {
+  return (
+    <>
+      {items.map((item) => {
+        const { id, name, image, price, quantity, description } = item;
+        return (
+          <div key={id}>
+            <div className="details">
+              <li>
+                <h3> {name}</h3>
+              </li>
+              <img src={image} alt={name} />
+              <p>Quantity: {quantity} </p>
+              <p>Price: ${price}</p>
+              <p>Description: {description} </p>
+            </div>
+          </div>
+        );
+      })}
 
+    </>
+  );
+}
+
+export default BasketView;
